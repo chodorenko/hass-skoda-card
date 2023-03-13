785c61f9ab92c76ec23ec62ca96b85f69fb19c15
Mikhail Khadarenka <chodorenko@mail.ru>
2023-03-13 16:43:21 +0300

    Replace define image URL

--- hass-skoda-card.js
+++ hass-skoda-card.js
@@ -44,7 +44,7 @@ class SkodaCard extends LitElement {
     if (!config.entities) {
       throw new Error('Please define a list of entities');
     }
-    this.imageurl = 'https://ip-modcwp.azureedge.net//modcwp3v5b20200903/0F0F-2ovtqWUlebk-6kVu2o9bdY-17NgmyXQTv.qVlH-yIC8G.6aPQ2EXMUzv-s1BZqeSWgmRyK69.kcuHUNDxO-DiSFcTLXOgdPZlAGIn-1080570studiovbeauty_connectview101281.png';
+//    this.imageurl = 'https://ip-modcwp.azureedge.net//modcwp3v5b20200903/0F0F-2ovtqWUlebk-6kVu2o9bdY-17NgmyXQTv.qVlH-yIC8G.6aPQ2EXMUzv-s1BZqeSWgmRyK69.kcuHUNDxO-DiSFcTLXOgdPZlAGIn-1080570studiovbeauty_connectview101281.png';
     this.config = config;
 
   }
